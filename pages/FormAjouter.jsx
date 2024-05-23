f3739dfc1e65a8a8640ee6788a004ea0c0e3e5f7
ZINGAClenn01 <zingaclennjovane@gmail.com>
2024-05-23 15:04:44 +0100

    fonction ajouter oki

--- pages/FormAjouter.jsx
+++ pages/FormAjouter.jsx
@@ -2,167 +2,98 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
- import axios from 'axios';
- import React from 'react';
-
-
-
+import React from 'react';
 import { useState } from 'react';
+import { doc, setDoc } from "firebase/firestore"; 
+import { db } from "../Firebase/Fibase"; // Assurez-vous d'importer votre instance de firebase/firestore
 
-export default function MonFormulaire()  {
+export default function MonFormulaire() {
   // Définition des états des champs de formulaire
-  const [image, setImage] = useState('');
-  const [title, setTitle] = useState('');
-  const [plus, setPlus] = useState('');
 
-  // Fonction de gestion de la soumission du formulaire
-  const soumettreFormulaire = async (event) => {
-    event.preventDefault(); // Empêche la soumission par défaut du formulaire
-    // Utilisez les valeurs des champs du formulaire
-    console.log('Url:', image);
-    console.log('Titre:', title);
-    console.log('Description:', plus);
-  
+  const [formData, setFormData] = useState({
+    title: '',
+    image: '',
+    plus: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      // Faites une requête POST vers votre API Strapi
-      const response = await axios.post('http://localhost:1337/api/posts', {
-        // Utilisez les mêmes clés que les noms de vos champs dans le formulaire
-        image: image,
-        title: title,
-        plus: plus,
-        // Autres champs de votre modèle de données à remplir si nécessaire
+      // Appel à la fonction pour ajouter l'élément dans Firestore
+      await ajouterElementDansFirestore(formData);
+      console.log('Article ajouté avec succès !');
+
+      // Réinitialisation du formulaire après l'ajout
+      setFormData({
+        title: '',
+        image: '',
+        plus: ''
+      });
+    } catch (error) {
+      console.error("Erreur lors de l'ajout de l'article : ", error);
+    }
+  };
+
+  const ajouterElementDansFirestore = async (data) => {
+    try {
+      // Ajout de l'élément dans la collection "chaussure"
+      await setDoc(doc(db, "chaussure", data.title), {
+        title: data.title,
+        image: data.image,
+        plus: data.plus
       });
   
-      console.log('Réponse de Strapi:', response.data);
-      // Ici vous pouvez traiter la réponse de Strapi si nécessaire
+      console.log("Document ajouté avec succès !");
     } catch (error) {
-      console.error('Erreur lors de la création de l\'élément dans Strapi:', error);
-      // Gérez l'erreur de manière appropriée
+      console.error("Erreur lors de l'ajout du document : ", error);
     }
   };
 
   return (
-    <form onSubmit={soumettreFormulaire}>
+    <form onSubmit={handleSubmit}>
       <Box
         sx={{
           width: 1000,
           maxWidth: '100%',
         }}
-
         className="boxform"
       >
         <TextField className='textfield'
           label="Image de l'article en Url"
           type="url"
-          value={image}
-          id='image'
-          onChange={(event) => setImage(event.target.value)}
+          id="image"
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
         />
-        <TextField className='textfield'
-          label="Titre de l'article"
-          type="text"
-          value={title}
-          id='title'
-          onChange={(event) => setTitle(event.target.value)}
-        />
+
         <TextField className='textfield'
           label="Description de l'article"
           type="text"
-          value={plus}
-          id='plus'
-          onChange={(event) => setPlus(event.target.value)}
+          id="plus"
+          name="plus"
+          value={formData.plus}
+          onChange={handleChange}
         />
-        <Button variant="contained" className='bouton-ajouter' type="submit">Creer un article</Button>
+
+        <TextField className='textfield'
+          label="Titre de l'article"
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        <Button variant="contained" className='bouton-ajouter' type="submit">Créer un article</Button>
       </Box>
     </form>
   );
 };
-
-
-
-
-
-// import { useState } from 'react';
-
-// export default function MonFormulaire() {
-//   const [title, setTitle] = useState("");
-//   const [image, setImage] = useState("");
-//   const [plus, setPlus] = useState("");
-//   const [user, setUser] = useState("");
-  
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     const data = {
-//       title: title,
-//       image: image,
-//       plus: plus,
-//       user: user
-//     }; console.log(title);
-//     console.log(plus);
-//     console.log(image);
-//     console.log(user);
-//     try {
-//       const response = await fetch('http://localhost:1337/api/posts', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//       });
-
-//       if (!response.ok) {
-//         throw new Error('Erreur lors de la création de l\'article');
-//       }
-      
-//       const responseData = await response.json();
-//       console.log('Réponse de l\'API:', responseData);
-
-//       setTitle("");
-//       setImage("");
-//       setPlus("");
-//       setUser("");
-      
-//     } catch (error) {
-//       console.error(error.message);
-//       console.log("CA PASSE PAS");
-//     }
-//   };
-  
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <label>
-//         Titre:
-//         <input
-//           type="text"
-//           value={title}
-//           onChange={(event) => setTitle(event.target.value)}
-//         />
-//       </label>
-//       <label>
-//         Image:
-//         <input
-//           type="url"
-//           value={image}
-//           onChange={(event) => setImage(event.target.value)}
-//         />
-//       </label>
-//       <label>
-//         Plus:
-//         <textarea
-//           value={plus}
-//           onChange={(event) => setPlus(event.target.value)}
-//         />
-//       </label>
-//       <label>
-//         User:
-//         <input
-//           type="text"
-//           value={user}
-//           onChange={(event) => setUser(event.target.value)}
-//         />
-//       </label>
-//       <button type="submit">Envoyer</button>
-//     </form>
-//   );
-// }
